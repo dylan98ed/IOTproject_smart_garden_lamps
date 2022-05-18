e4b8965da2d02c90899ec3f5263751b22a183c1f
Dylan Quiroga <dyalanquiroga98@gmail.com>
2022-05-18 14:11:31 -0300

    correction of SQL commands

--- lamp2.php
+++ lamp2.php
@@ -13,4 +13,8 @@ $conn=new conexion();
 $queryUPDATE="UPDATE `estado` SET `lamparados` = '$lamp2' WHERE `estado`.`dispositivo` = '$dispositivo';";
 $update= mysqli_query($conn->conectardb(),$queryUPDATE);
 
+//actualizar tabla HISTORICO
+$queryINSERT="INSERT INTO `historico` (`dispositivo`, `lamparauno`, `lamparados`, `fechaRegistro`) VALUES ('$dispositivo', '$lamp1', '$lamp2', NOW());";
+$insert= mysqli_query($conn->conectardb(),$queryINSERT);
+
 ?>
--- send.php
+++ send.php
@@ -10,17 +10,6 @@ $lamp2=$_GET['lamparados'];
 
 $conn=new conexion();
 
-
-//actualizar tabla ESTADO
-//$queryUPDATE="UPDATE `estado` SET `lamparauno` = '$lamp1', `lamparados` = '$lamp2' WHERE `estado`.`dispositivo` = '$dispositivo';";
-//$update= mysqli_query($conn->conectardb(),$queryUPDATE);
-
-//actualizar tabla HISTORICO
-$queryINSERT="INSERT INTO `historico` (`dispositivo`, `lamparauno`, `lamparados`, `fechaRegistro`) VALUES ('$dispositivo', '$lamp1', '$lamp2', NOW());";
-$insert= mysqli_query($conn->conectardb(),$queryINSERT);
-
-
-
 //filtrar el estado de la tarjeta
 $querySELECT="SELECT`lamparauno`,`lamparados` FROM `estado` WHERE `dispositivo`= 'node1';";
 $result= mysqli_query($conn->conectardb(),$querySELECT);
